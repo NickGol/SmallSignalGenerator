d205b3fe22d706d6257c1f652d44d10018157ee0
NickGol <43709923+NickGol@users.noreply.github.com>
2020-03-14 22:54:08 +0300

    Change project structure First tests added to try with Travis CI

---
+++ src/test/java/TestSinusGenerator.java
@@ -0,0 +1,49 @@
+import SinusGeneratorPackage.SinusGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestSinusGenerator {
+
+    private int capacity = 55;
+    private SinusGenerator sinusGenerator;
+
+    @Before
+    public void load() {
+        sinusGenerator = new SinusGenerator(capacity);
+    }
+
+    @Test
+    public void checkGetterFunction_1() {
+        Float[] firstStep = sinusGenerator.getSignalData();
+        assertEquals(firstStep.length, capacity);
+    }
+
+    @Test
+    public void checkGetterFunction_2() {
+        Float[] firstStep = sinusGenerator.getSignalData(25);
+        assertEquals(firstStep.length, 25);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkGetterFunctionExceptionBigValue() {
+        Float[] firstStep = sinusGenerator.getSignalData(capacity+1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkGetterFunctionExceptionNegativeValue() {
+        Float[] firstStep = sinusGenerator.getSignalData(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkConstructorExceptionZeroValue() {
+        SinusGenerator sinusGenerator = new SinusGenerator(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkConstructorExceptionNegativeValue() {
+        SinusGenerator sinusGenerator = new SinusGenerator(-1);
+    }
+}
