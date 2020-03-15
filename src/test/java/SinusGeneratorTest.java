369c73fec940ff347519bb6777fe3e76e8e155cf
NickGol <43709923+NickGol@users.noreply.github.com>
2020-03-15 23:02:56 +0300

    Change project structure First tests added to try with Travis CI

--- src/test/java/SinusGeneratorTest.java
+++ src/test/java/SinusGeneratorTest.java
@@ -17,33 +17,39 @@ public class SinusGeneratorTest {
 
     @Test
     public void checkGetterFunction_1() {
+
         Float[] firstStep = sinusGenerator.getSignalData();
         assertEquals(firstStep.length, capacity);
     }
 
     @Test
     public void checkGetterFunction_2() {
+
         Float[] firstStep = sinusGenerator.getSignalData(25);
         assertEquals(firstStep.length, 25);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void checkGetterFunctionExceptionBigValue() {
+
         Float[] firstStep = sinusGenerator.getSignalData(capacity+1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void checkGetterFunctionExceptionNegativeValue() {
+
         Float[] firstStep = sinusGenerator.getSignalData(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void checkConstructorExceptionZeroValue() {
+
         SinusGenerator sinusGenerator = new SinusGenerator(0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void checkConstructorExceptionNegativeValue() {
+
         SinusGenerator sinusGenerator = new SinusGenerator(-1);
     }
 }
